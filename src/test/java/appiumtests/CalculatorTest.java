431cc1a183b917ad98c6b81aca2c76e6324f80b2
Aman Khan <pamanullahkhan.aman@gmail.com>
2021-01-04 18:50:16 +0530

    Added testing dependencies and updated code for calculation

--- src/test/java/appiumtests/CalculatorTest.java
+++ src/test/java/appiumtests/CalculatorTest.java
@@ -3,6 +3,7 @@ package appiumtests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,14 +13,23 @@ import java.net.URL;
 
 public class CalculatorTest {
 
+    public static String CALC_KEY_TWO = "com.android.calculator2:id/digit_2";
+    public static String CALC_KEY_THREE = "com.android.calculator2:id/digit_3";
+    public static String CALC_KEY_PLUS = "com.android.calculator2:id/op_add";
+    public static String CALC_KEY_EQUALS = "com.android.calculator2:id/eq";
+    public static String CALC_KEY_RESULT = "com.android.calculator2:id/result";
+
     //WebDriver webDriver;
     static AppiumDriver<MobileElement> appiumDriver;
     //AndroidDriver androidDriver;
 
+    public int value;
+
     public static void main(String[] args) {
         try {
             System.out.println("---Connect Appium and Open Calculator Application---");
-            openCalculator();
+            Assert.assertEquals("Calculated value result is not same",5, openCalculator());
+            System.out.println("---Validation Successful---");
         } catch (MalformedURLException e) {
             e.printStackTrace();
             e.getCause();
@@ -27,7 +37,7 @@ public class CalculatorTest {
         }
     }
 
-    public static void openCalculator() throws MalformedURLException {
+    public static int openCalculator() throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
 
         System.out.println("---Device Connection Details---");
@@ -49,14 +59,16 @@ public class CalculatorTest {
 
         System.out.println("-----Application Started-----");
 
-        MobileElement two = appiumDriver.findElement(By.id("com.android.calculator2:id/digit_2"));
-        MobileElement plus = appiumDriver.findElement(By.id("com.android.calculator2:id/op_add"));
-        MobileElement three = appiumDriver.findElement(By.id("com.android.calculator2:id/digit_3"));
-        MobileElement equals = appiumDriver.findElement(By.id("com.android.calculator2:id/eq"));
+        MobileElement two = appiumDriver.findElement(By.id(CALC_KEY_TWO));
+        MobileElement plus = appiumDriver.findElement(By.id(CALC_KEY_PLUS));
+        MobileElement three = appiumDriver.findElement(By.id(CALC_KEY_THREE));
+        MobileElement equals = appiumDriver.findElement(By.id(CALC_KEY_EQUALS));
+        MobileElement result = appiumDriver.findElement(By.id(CALC_KEY_RESULT));
 
         two.click();
         plus.click();
         three.click();
         equals.click();
+        return Integer.parseInt(result.getText());
     }
 }
